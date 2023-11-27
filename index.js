82db3365870fb4e3f5f5763dd22bd51930c80053
Adnan4141 <adnanh4141@gmail.com>
2023-11-27 11:00:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const cors = require("cors");
+const app = express()
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+
+
+app.use(express.json());
+app.use(cors());
+const uri = "mongodb+srv://NewCarHub:newcarhub@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        const vehicleCollection = client.db("rental-service").collection("vehicles");
+        app.post('/add-a-vehicle', async (req, res) => {
+            const vehicle = req.body;
+            const result = await vehicleCollection.insertOne(vehicle);
+            console.log(vehicle)
+            res.send(result);
+        })
+        app.get('/all-vehicles', async (req, res) => {
+
+            const result = await vehicleCollection.find().toArray();
+            res.send(result);
+
+        })
+    }
+    
+    finally {
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Helloo World')
+})
+
+app.get('/Home', (req, res) => {
+    res.send(user)
+})
+
+
+app.listen(port, () => {
+    console.log(` http://localhost:${port}/ `)
+})
+
+
+
+//nodemon index.js
+// npm i -g nodemon cors express mongodb
+// npm install -g nodemon
+
+
